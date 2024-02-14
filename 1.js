f64957ed99a7aca2d8213c37a9592b00102853bb
kanishk1122 <kansihk21soni@gmail.com>
2024-02-14 15:23:52 +0530

    commted succesfully

---
+++ 1.js
@@ -0,0 +1,7 @@
+function fa1(){
+ window.location.href="2.html"
+}
+setTimeout (function fa1(){
+
+
+},2500)
---
+++ 2.js
@@ -0,0 +1,10 @@
+function fa1(){
+    window.location.href="1.html"
+}
+   setTimeout (function fa1(){
+
+
+   },2500)
+   let check=document.getElementById("check")
+   check.checked=true;
+   
